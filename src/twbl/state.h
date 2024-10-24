fee5a58cda346f00223eb57df2753f8a6446eeb2
ChillerDragon <chillerdragon@gmail.com>
2024-10-24 15:36:24 +0800

    fix multi line comment warning

--- src/twbl/state.h
+++ src/twbl/state.h
@@ -6,12 +6,12 @@
 class CTwblPersistentState
 {
 public:
-	//  _
-	// | |__   __ _ ___  ___
-	// | '_ \ / _` / __|/ _ \
-	// | |_) | (_| \__ \  __/
-	// |_.__/ \__,_|___/\___|
-	//
+	/*   _
+	 *  | |__   __ _ ___  ___
+	 *  | '_ \ / _` / __|/ _ \
+	 *  | |_) | (_| \__ \  __/
+	 *  |_.__/ \__,_|___/\___|
+	 */
 
 	// how many times the twbl tick method has been called
 	int m_TwblTicks = 0;
@@ -19,12 +19,12 @@ public:
 	// used to track fire state to increment it every tick
 	int m_Fire = 0;
 
-	//                 _
-	//   ___ _   _ ___| |_ ___  _ __ ___
-	//  / __| | | / __| __/ _ \| '_ ` _ \
-	// | (__| |_| \__ \ || (_) | | | | | |
-	//  \___|\__,_|___/\__\___/|_| |_| |_|
-	//
+	/*                  _
+	 *    ___ _   _ ___| |_ ___  _ __ ___
+	 *   / __| | | / __| __/ _ \| '_ ` _ \
+	 *  | (__| |_| \__ \ || (_) | | | | | |
+	 *   \___|\__,_|___/\__\___/|_| |_| |_|
+	 */
 };
 
 static const constexpr size_t MIN_TWBL_STATE_SIZE = sizeof(CTwblPersistentState);
