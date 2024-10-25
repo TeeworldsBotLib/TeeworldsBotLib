00b78e332319a5f0a7985522d4524bd76088fc4b
ChillerDragon <chillerdragon@gmail.com>
2024-10-25 14:36:50 +0800

    git conflict comment for callback extensions

--- src/twbl/callback_ctx.h
+++ src/twbl/callback_ctx.h
@@ -1,11 +1,20 @@
 #ifndef TWBL_SRC_TWBL_CALLBACK_CTX_H
 #define TWBL_SRC_TWBL_CALLBACK_CTX_H
 
+#include <twbl/teeworlds/base/vmath.h>
+
 namespace TWBL {
 
 class CCallbackCtx
 {
 public:
+	/*   _
+	 *  | |__   __ _ ___  ___
+	 *  | '_ \ / _` / __|/ _ \
+	 *  | |_) | (_| \__ \  __/
+	 *  |_.__/ \__,_|___/\___|
+	 */
+
 	virtual ~CCallbackCtx() = default;
 	virtual void SendChat(int Team, const char *pText) = 0;
 	virtual void Die() = 0;
@@ -13,6 +22,13 @@ public:
 
 	int m_ClientId = 0;
 	int GetCid() { return m_ClientId; }
+
+	/*                  _
+	 *    ___ _   _ ___| |_ ___  _ __ ___
+	 *   / __| | | / __| __/ _ \| '_ ` _ \
+	 *  | (__| |_| \__ \ || (_) | | | | | |
+	 *   \___|\__,_|___/\__\___/|_| |_| |_|
+	 */
 };
 
 }; // namespace TWBL
