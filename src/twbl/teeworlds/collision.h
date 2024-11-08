12f8d4c500bb902179506c6794a5fc12bf0b9451
ChillerDragon <chillerdragon@gmail.com>
2024-11-08 10:57:42 +0800

    Add IntersectLine to collision class

--- src/twbl/teeworlds/collision.h
+++ src/twbl/teeworlds/collision.h
@@ -1,6 +1,8 @@
 #ifndef TWBL_SRC_TWBL_COLLISION_H
 #define TWBL_SRC_TWBL_COLLISION_H
 
+#include <twbl/teeworlds/base/vmath.h>
+
 #if __has_include(<game/collision.h>)
 #include <game/collision.h>
 #else
@@ -8,6 +10,7 @@ class CCollision
 {
 public:
 	int GetTile(int x, int y) const { return -404; }
+	int IntersectLine(vec2 Pos0, vec2 Pos1, vec2 *pOutCollision, vec2 *pOutBeforeCollision) const { return -404; }
 };
 
 #endif
