c03c3f6428a7ace8d4f858007ef217476cc76789
ChillerDragon <chillerdragon@gmail.com>
2024-11-10 15:04:26 +0800

    ddnet only collision api

--- src/twbl/teeworlds/collision.h
+++ src/twbl/teeworlds/collision.h
@@ -11,6 +11,11 @@ class CCollision
 public:
 	int GetTile(int x, int y) const { return -404; }
 	int IntersectLine(vec2 Pos0, vec2 Pos1, vec2 *pOutCollision, vec2 *pOutBeforeCollision) const { return -404; }
+
+	// TODO: this is ddnet only
+	int GetPureMapIndex(float x, float y) const { return 0; }
+	int GetPureMapIndex(vec2 Pos) const { return GetPureMapIndex(Pos.x, Pos.y); }
+	int GetTileIndex(int Index) const { return 0; }
 };
 
 #endif
